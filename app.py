c05accd6bf1ed379239847dd920fcaae733948fe
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-04-01 09:42:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_users_data():
     cursor = conn.cursor()
 
     
-    cursor.execute("SELECT name, email FROM users")  # Replace 'users' with
+    cursor.execute("SELECT name, email FROM users")  # Replace 'users'
     existing_users = cursor.fetchall()
 
     conn.close()
