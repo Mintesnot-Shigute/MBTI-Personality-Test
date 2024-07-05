47220e0dc7727731411b6c88dada51361b38830b
Mintesnot Shigute <81554903+Mintesnot-Shigute@users.noreply.github.com>
2024-07-05 09:00:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def check_credentials():
     data = request.get_json()
     name = data.get('name')
     email = data.get('email')
-    conn = sqlite3.connect('user_data.db')  # Replace with yo
+    conn = sqlite3.connect('user_data.db')  # Replace with y
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM users WHERE name = ? AND email = ?", (name, email))
